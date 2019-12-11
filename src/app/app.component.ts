c56ec5af489c26a77a0539394423d7cbbcde613d
lsneefirst <lsommereux@gmail.com>
2019-12-11 10:32:53 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component,OnInit } from '@angular/core';
-declare var jQuery: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
 
-
- OnInit(){
+  ngOnInit(){
     (function ($) {
+      $(document).ready(function(){
         console.log("Hello from jQuery!");
+      });
     })(jQuery);
   }
-  }
+}
